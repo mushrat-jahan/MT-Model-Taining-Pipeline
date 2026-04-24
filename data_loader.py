876bfa7228071ab120826cf43cc0c49e7e8a7f4e
mushrat_jahan <mushratjahan57@gmail.com>
2026-04-24 15:51:45 +0600

    updated data processing file

--- data_loader.py
+++ data_loader.py
@@ -1,7 +1,8 @@
-from data_processing import DataLoader
+# from data_processing import DataLoader
+from test import DataLoader
 
-loader = DataLoader("D:\\office\\EBLICT\\Model training for MT\\(AcceptedMachineTranslationData-2026-04-22.csv")
+loader = DataLoader("D:\\office\\EBLICT\\Model training for MT\\Untitled spreadsheet - Sheet1.csv")
 print(loader.get_cleaned_data().head())
 
-loader.save_cleaned_data("D:\\office\\EBLICT\\Model training for MT\\output_clean.csv", index=False)
+loader.save_cleaned_data("D:\\office\\EBLICT\\Model training for MT\\output_clean11.csv", index=False)
 
--- data_processing.py
+++ data_processing.py
@@ -7,16 +7,21 @@ class DataLoader:
         self._data_load_and_clean()
 
     def _data_load_and_clean(self):
-        #load row csv file
+        # load raw csv file
         df = pd.read_csv(self.filepath)
 
-        #keep desired columns
-        desired_cols = ["text_data", "final_text_version", "source_language", "target_language", "final_text_data"]
+        # Fill empty values in final_text_version using text_data
+        df["final_text_data"] = df["final_text_data"].replace("", pd.NA)
+        df["source_text"] = df["final_text_data"].fillna(df["text_data"])
+
+        # keep desired columns
+        desired_cols = ["source_text", "final_text_version", "source_language", "target_language"]
         cols_to_drop = [col for col in df.columns if col not in desired_cols]
-        df = df.drop(columns= cols_to_drop)
-        
-        # rename the target colmun
+        df = df.drop(columns=cols_to_drop)
+
+        # Rename target column
         df = df.rename(columns={"final_text_version": "target_text"})
+
         self.df = df
 
     def get_cleaned_data(self):
@@ -24,7 +29,6 @@ class DataLoader:
     
     def save_cleaned_data(self, output_path: str, index: bool = False):
         self.df.to_csv(output_path, index=index)
-
 
 
 
