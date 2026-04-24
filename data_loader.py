1833940fa8b56e0d26ba37f07d5649a7cf830d64
mushrat_jahan <mushratjahan57@gmail.com>
2026-04-24 16:17:22 +0600

    data loader updated

--- data_loader.py
+++ data_loader.py
@@ -1,8 +1,7 @@
-# from data_processing import DataLoader
-from test import DataLoader
+from data_processing import DataLoader
 
-loader = DataLoader("D:\\office\\EBLICT\\Model training for MT\\Untitled spreadsheet - Sheet1.csv")
+loader = DataLoader("Model training for MT\\Dataset\\input5k.csv")
 print(loader.get_cleaned_data().head())
 
-loader.save_cleaned_data("D:\\office\\EBLICT\\Model training for MT\\output_clean11.csv", index=False)
+loader.save_cleaned_data("Model training for MT\\Dataset\\processed_data.csv", index=False)
 
